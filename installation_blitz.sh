0e905aea649d4da0c6a3fbe704542552b8157e59
brigerodev <golden.notch.creeper@gmail.com>
2025-11-03 00:54:35 -0800

    add specific branch

--- installation_blitz.sh
+++ installation_blitz.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-
+BRANCH_NAME="merge-backend"
 DEFAULT_PI_NAME="blitz-pi-random-name-1234"
 
 cd ~/Documents
 
-git clone https://github.com/PinewoodRobotics/B.L.I.T.Z.git
+git clone -b "$BRANCH_NAME" https://github.com/PinewoodRobotics/B.L.I.T.Z.git
 cd B.L.I.T.Z
 git submodule update --init --recursive
 
